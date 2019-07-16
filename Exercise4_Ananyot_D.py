f12ddb6bbdcc19353c47280720e7a96dfc353914
Ananyot <kav_ananyos@hotmail.com>
2019-07-16 14:28:51 +0700

    Added Ex4

---
+++ Exercise4_Ananyot_D.py
@@ -0,0 +1,10 @@
+FE = 80
+GB = 65.5
+ICS = 70
+CP = 85
+
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
